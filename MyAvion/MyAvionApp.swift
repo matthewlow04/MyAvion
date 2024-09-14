f7208e4b65481c35ff68e6ecfc4a35d8dd8ff351
Matthew Low <matthew7419@gmail.com>
2024-09-14 16:57:21 -0400

    Initial Commit

---
+++ MyAvion/MyAvionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAvionApp.swift
+//  MyAvion
+//
+//  Created by Matthew Low on 2024-09-14.
+//
+
+import SwiftUI
+
+@main
+struct MyAvionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
